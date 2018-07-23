e63d3e3ff748a5f749786cb8c3ffb3dc2048e23f
delaney washington <delaneywashington123@gmail.com>
2018-07-23 20:24:14 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -23,4 +23,4 @@ def skipstep
     puts "Lift right arm up and point right elbow diagonally to the right."
     puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
   end
-  
+end
