e01089e253540c44558c5d3b4daab3254c6b7582
delaney washington <delaneywashington123@gmail.com>
2018-07-23 20:02:48 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,8 @@
 
 # Code your methods below
+def lower body
+  puts plate legs far apart, bend knees slightly and keep posture loose
+  puts lift right foot
+  puts return right foot to the ground
+  puts finishing with a small skip-step backward
+end 
